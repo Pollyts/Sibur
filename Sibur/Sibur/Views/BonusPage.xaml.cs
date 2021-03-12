1c2c25a905d1a1af8df660aa805704353d5062e4
Polina Tselishcheva <polia.tselisheva@mail.ru>
2021-03-13 03:36:18 +0500

    Добавила страницу с бонусом

---
+++ Sibur/Sibur/Views/BonusPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sibur.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BonusPage : ContentPage
+    {
+        public string NumberOfDays { get; set; }
+        public BonusPage(string nday)
+        {
+            InitializeComponent();
+            NumberOfDays = nday;
+            BindingContext = this;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
